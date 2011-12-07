259deefaa156cbeb0b74a5365a828ce014954f2c
John Anderson <sontek@gmail.com>
2011-12-07 15:55:17 -0600

    added tests for the schemas

---
+++ pyramid_signup/tests/test_schemas.py
@@ -0,0 +1,35 @@
+from pyramid_signup.tests import UnitTestBase
+from pyramid_signup.schemas import LoginSchema
+from colander import Invalid
+
+class TestModels(UnitTestBase):
+    def test_valid_login_schema(self):
+        request = self.get_csrf_request(post={
+            'Username': 'sontek',
+            'Password': 'password',
+            })
+
+        schema = LoginSchema().bind(request=request)
+
+        result = schema.deserialize(request.POST)
+
+        assert result['Username'] == 'sontek'
+        assert result['Password'] == 'password'
+        assert result['csrf_token'] != None
+
+    def test_invalid_login_schema(self):
+        request = self.get_csrf_request()
+        schema = LoginSchema().bind(request=request)
+
+        def deserialize_empty():
+            try:
+                schema.deserialize({})
+            except Invalid as exc:
+                assert len(exc.children) == 3
+
+                for child in exc.children:
+                    assert child.msg == u'Required'
+
+                raise
+
+        self.assertRaises(Invalid, deserialize_empty)
