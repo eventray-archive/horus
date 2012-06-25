c6b61c85df4a223c48a11f15abc6bc3617284782
John Anderson <sontek@gmail.com>
2012-06-25 07:15:05 -0500

    version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.3'
+      , version='0.4'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
