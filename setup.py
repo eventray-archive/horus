d27b5b08b18851e45a947b9de0feb3db0f44f2e8
John Anderson <sontek@gmail.com>
2012-07-29 17:13:41 -0500

    bump series

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.9.3'
+      , version='0.9.4'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
