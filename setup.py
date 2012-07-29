8d60356cf9eb44ab15da626fc0889f6eb3ee7eb3
John Anderson <sontek@gmail.com>
2012-07-29 17:29:06 -0500

    cleaned up the manifest

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.9.4'
+      , version='0.9.8'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
