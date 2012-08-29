3a098110c02336908e286df35f799b562ebc58a3
John Anderson <sontek@gmail.com>
2012-08-28 21:10:41 -0400

    bumped version for pypi release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ requires = [
 ]
 
 setup(name='horus'
-      , version='0.9.8'
+      , version='0.9.9'
       , description='Generic user registration for pyramid'
       , long_description=README + '\n\n' +  CHANGES
       , classifiers=[
